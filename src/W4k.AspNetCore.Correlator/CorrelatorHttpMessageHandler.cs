c723f849e4d4ca1fd3973ebcee96b8be5a17d27d
Zdenek Havlin <wdolek@hotmail.com>
2018-09-23 21:42:34 +0200

    Introduce `CorrelatorHttpMessageHandler` for propagating `X-Correlation-ID` to other requests (if used)

---
+++ src/W4k.AspNetCore.Correlator/CorrelatorHttpMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using W4k.AspNetCore.Correlator.Extensions;
+
+namespace W4k.AspNetCore.Correlator
+{
+    /// <summary>
+    /// Delegating HTTP message handler handling correlation ID in outgoing request.
+    /// </summary>
+    public class CorrelatorHttpMessageHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelatorHttpMessageHandler"/> class.
+        /// </summary>
+        /// <param name="contextAccessor">HTTP context accessor.</param>
+        public CorrelatorHttpMessageHandler(IHttpContextAccessor contextAccessor)
+            : base()
+        {
+            _contextAccessor = contextAccessor ?? throw new System.ArgumentNullException(nameof(contextAccessor));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelatorHttpMessageHandler"/> class.
+        /// </summary>
+        /// <param name="innerHandler">Inner HTTP message handler.</param>
+        /// <param name="contextAccessor">HTTP context accessor.</param>
+        public CorrelatorHttpMessageHandler(HttpMessageHandler innerHandler, IHttpContextAccessor contextAccessor)
+            : base(innerHandler)
+        {
+            _contextAccessor = contextAccessor ?? throw new System.ArgumentNullException(nameof(contextAccessor));
+        }
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            request.AddHeaderIfNotSet(HttpHeaders.CorrelationId, _contextAccessor.HttpContext?.TraceIdentifier);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/W4k.AspNetCore.Correlator/Extensions/HttpRequestMessageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+
+namespace W4k.AspNetCore.Correlator.Extensions
+{
+    /// <summary>
+    /// HTTP request message extensions.
+    /// </summary>
+    public static class HttpRequestMessageExtensions
+    {
+        /// <summary>
+        /// Adds header to request if not set.
+        /// </summary>
+        /// <param name="request">HTTP request message.</param>
+        /// <param name="headerName">HTTP header name.</param>
+        /// <param name="value">Header value.</param>
+        /// <returns>
+        /// HTTP request message with header set.
+        /// </returns>
+        public static HttpRequestMessage AddHeaderIfNotSet(
+            this HttpRequestMessage request,
+            string headerName,
+            string value)
+        {
+            if (request == null || string.IsNullOrEmpty(value) || request.Headers.Contains(headerName))
+            {
+                return request;
+            }
+
+            request.Headers.Add(headerName, value);
+
+            return request;
+        }
+    }
+}
