6dc20194d352cfb2ec145926601e9e1a2d959715
Zdenek Havlin <wdolek@hotmail.com>
2018-09-23 21:43:18 +0200

    Add `IServiceCollection` extension for registering all required components

---
+++ src/W4k.AspNetCore.Correlator/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace W4k.AspNetCore.Correlator.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCorrelator(this IServiceCollection services)
+        {
+            // may be already registered
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            return services.AddSingleton<CorrelatorHttpMessageHandler>();
+        }
+    }
+}
