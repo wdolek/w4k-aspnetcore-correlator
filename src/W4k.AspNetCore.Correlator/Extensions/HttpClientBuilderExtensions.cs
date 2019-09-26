86d20814d00478e168ff0bdd3c54539b121295fd
Zdenek Havlin <wdolek@hotmail.com>
2019-09-26 10:21:40 +0200

    Add `WithCorrelation` extension method on `IHttpClientBuilder` for easier hookup of `CorrelatorHttpMessageHandler`

---
+++ src/W4k.AspNetCore.Correlator/Extensions/HttpClientBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace W4k.AspNetCore.Correlator.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="IHttpClientBuilder"/>.
+    /// </summary>
+    public static class HttpClientBuilderExtensions
+    {
+        /// <summary>
+        /// Configures HTTP client with <see cref="CorrelatorHttpMessageHandler"/>.
+        /// </summary>
+        /// <param name="builder">HTTP client builder.</param>
+        /// <returns>
+        /// HTTP client builder configured to use correlator message handler.
+        /// </returns>
+        public static IHttpClientBuilder WithCorrelation(this IHttpClientBuilder builder) =>
+            builder.AddHttpMessageHandler<CorrelatorHttpMessageHandler>();
+    }
+}
