77e75fb6aba5bd087474eda1dc65c0b1435c5998
Zdenek Havlin <wdolek@hotmail.com>
2019-10-15 22:33:24 +0200

    Introduce sample project (`netcore3.0`)

---
+++ examples/W4k.AspNetCore.Correlator.Example.NetCore30/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace W4k.AspNetCore.Correlator.Example.NetCore30
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ examples/W4k.AspNetCore.Correlator.Example.NetCore30/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using W4k.AspNetCore.Correlator.Extensions;
+using W4k.AspNetCore.Correlator.Options;
+
+namespace W4k.AspNetCore.Correlator.Example.NetCore30
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add correlator with additional setting
+            services.AddCorrelator(options =>
+            {
+                // forward, return using `Request-Id` HTTP header
+                options.Forward = PropagationSettings.PropagateAs("Request-Id");
+                options.Emit = PropagationSettings.PropagateAs("Request-Id");
+            });
+
+            // add named HTTP client
+            services
+                .AddHttpClient("DummyClient")
+                .WithCorrelation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // use Correlator middleware
+            app.UseCorrelator();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
