612f1b8f645ff1e6a24298e4f3d6d4327294825a
Zdenek Havlin <wdolek@hotmail.com>
2020-08-10 22:43:51 +0200

    Introduce `ICorrelationEmitter`, simplify middleware

---
+++ src/W4k.AspNetCore.Correlator/CorrelationEmitter.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using W4k.AspNetCore.Correlator.Context;
+using W4k.AspNetCore.Correlator.Context.Types;
+using W4k.AspNetCore.Correlator.Extensions;
+using W4k.AspNetCore.Correlator.Options;
+
+namespace W4k.AspNetCore.Correlator
+{
+    internal class CorrelationEmitter : ICorrelationEmitter
+    {
+        private readonly CorrelatorOptions _options;
+        private readonly ICorrelationContextAccessor _contextAccessor;
+
+        public CorrelationEmitter(
+            IOptions<CorrelatorOptions> options,
+            ICorrelationContextAccessor correlationContextAccessor)
+        {
+            _options = options.Value;
+            _contextAccessor = correlationContextAccessor;
+        }
+
+        /// <inheritdoc/>
+        public Task Emit(HttpContext httpContext)
+        {
+            if (_options.Emit.Settings == HeaderPropagation.NoPropagation)
+            {
+                return Task.CompletedTask;
+            }
+
+            var correlationContext = _contextAccessor.CorrelationContext;
+
+            string? responseHeaderName = GetResponseHeaderName(correlationContext);
+            if (responseHeaderName != null)
+            {
+                httpContext.Response.Headers.AddHeaderIfNotSet(
+                    responseHeaderName,
+                    correlationContext.CorrelationId);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private string? GetResponseHeaderName(CorrelationContext correlationContext)
+        {
+            if (_options.Emit.Settings == HeaderPropagation.UsePredefinedHeaderName)
+            {
+                return _options.Emit.HeaderName;
+            }
+
+            if (_options.Emit.Settings == HeaderPropagation.KeepIncomingHeaderName
+                && correlationContext is RequestCorrelationContext requestCorrelationContext)
+            {
+                return requestCorrelationContext.Header;
+            }
+
+            return null;
+        }
+    }
+}
--- src/W4k.AspNetCore.Correlator/CorrelatorMiddleware.cs
+++ src/W4k.AspNetCore.Correlator/CorrelatorMiddleware.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
 using W4k.AspNetCore.Correlator.Context;
-using W4k.AspNetCore.Correlator.Context.Types;
 using W4k.AspNetCore.Correlator.Extensions;
-using W4k.AspNetCore.Correlator.Options;
 
 namespace W4k.AspNetCore.Correlator
 {
@@ -17,33 +12,27 @@ namespace W4k.AspNetCore.Correlator
     internal class CorrelatorMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly CorrelatorOptions _options;
         private readonly ICorrelationContextFactory _contextFactory;
         private readonly ICorrelationContextContainer _contextContainer;
+        private readonly ICorrelationEmitter _emitter;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CorrelatorMiddleware"/> class.
         /// </summary>
         /// <param name="next">Delegate representing the next middleware in the request pipeline.</param>
-        /// <param name="options">Correlator options.</param>
         /// <param name="correlationContextFactory">Correlation context factory.</param>
         /// <param name="correlationContextContainer">Correlation context container.</param>
+        /// <param name="correlationEmitter">Correlation emitter.</param>
         public CorrelatorMiddleware(
             RequestDelegate next,
-            IOptions<CorrelatorOptions> options,
             ICorrelationContextFactory correlationContextFactory,
-            ICorrelationContextContainer correlationContextContainer)
+            ICorrelationContextContainer correlationContextContainer,
+            ICorrelationEmitter correlationEmitter)
         {
-            _next = next ?? throw new ArgumentNullException(nameof(next));
-
-            _contextFactory = correlationContextFactory
-                ?? throw new ArgumentNullException(nameof(correlationContextFactory));
-
-            _contextContainer = correlationContextContainer
-                ?? throw new ArgumentNullException(nameof(correlationContextContainer));
-
-            _ = options ?? throw new ArgumentNullException(nameof(options));
-            _options = options.Value;
+            _next = next;
+            _contextFactory = correlationContextFactory;
+            _contextContainer = correlationContextContainer;
+            _emitter = correlationEmitter;
         }
 
         /// <summary>
@@ -59,55 +48,10 @@ namespace W4k.AspNetCore.Correlator
             var correlationContext = _contextFactory.CreateContext(httpContext);
             using (_contextContainer.CreateScope(correlationContext))
             {
-                if (_options.Emit != PropagationSettings.NoPropagation)
-                {
-                    httpContext.Response.OnStarting(
-                        state => EmitCorrelationId((HttpContext)state, correlationContext, _options),
-                        httpContext);
-                }
+                httpContext.Response.OnStarting(ctx => _emitter.Emit((HttpContext)ctx), httpContext);
 
                 await _next.Invoke(httpContext.WithCorrelationId(correlationContext.CorrelationId));
             }
-        }
-
-        /// <summary>
-        /// Adds correlation ID to response headers.
-        /// </summary>
-        /// <param name="httpContext">HTTP context.</param>
-        /// <param name="correlationContext">Correlation context.</param>
-        /// <param name="options">Correlator options.</param>
-        /// <returns>
-        /// Task defining action of emitting correlation ID.
-        /// </returns>
-        private static Task EmitCorrelationId(
-            HttpContext httpContext,
-            CorrelationContext correlationContext,
-            CorrelatorOptions options)
-        {
-            var responseHeaderName = (options.Emit.Settings, correlationContext) switch
-            {
-                // emit correlation ID with predefined header name
-                (HeaderPropagation.UsePredefinedHeaderName, _) =>
-                    options.Emit.HeaderName,
-
-                // emit correlation ID with same header name as we received it
-                (HeaderPropagation.KeepIncomingHeaderName, RequestCorrelationContext requestCorrelationContext) =>
-                    requestCorrelationContext.Header,
-
-                // emit correlation ID with first known header name when correlation has been generated
-                (HeaderPropagation.KeepIncomingHeaderName, GeneratedCorrelationContext _) =>
-                    options.ReadFrom.FirstOrDefault(),
-
-                // correlation ID not received nor generated
-                _ => null
-            };
-
-            if (responseHeaderName is object)
-            {
-                httpContext.Response.Headers.AddHeaderIfNotSet(responseHeaderName, correlationContext.CorrelationId);
-            }
-
-            return Task.CompletedTask;
         }
     }
 }
