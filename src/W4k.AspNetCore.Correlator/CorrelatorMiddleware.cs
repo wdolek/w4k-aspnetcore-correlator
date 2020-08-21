f602005afafbc31180cbab175a93bdb2eae8844f
Zdenek Havlin <wdolek@hotmail.com>
2020-08-22 00:27:49 +0200

    Allow user to configure whether correlation ID is included in logging scope (#14)
    * Allow user to configure whether correlation ID is included in logging scope
    
    * Use `ILogger<T>` for logger
    
    * Fix replacing `TraceIdentifier`, Fix tests

--- src/W4k.AspNetCore.Correlator/CorrelatorMiddleware.cs
+++ src/W4k.AspNetCore.Correlator/CorrelatorMiddleware.cs
@@ -1,9 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using W4k.AspNetCore.Correlator.Context;
-using W4k.AspNetCore.Correlator.Extensions;
 using W4k.AspNetCore.Correlator.Options;
 
 namespace W4k.AspNetCore.Correlator
@@ -18,6 +19,7 @@ namespace W4k.AspNetCore.Correlator
         private readonly ICorrelationContextFactory _contextFactory;
         private readonly ICorrelationContextContainer _contextContainer;
         private readonly ICorrelationEmitter _emitter;
+        private readonly ILogger _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CorrelatorMiddleware"/> class.
@@ -27,18 +29,21 @@ namespace W4k.AspNetCore.Correlator
         /// <param name="correlationContextFactory">Correlation context factory.</param>
         /// <param name="correlationContextContainer">Correlation context container.</param>
         /// <param name="correlationEmitter">Correlation emitter.</param>
+        /// <param name="logger">Logger.</param>
         public CorrelatorMiddleware(
             RequestDelegate next,
             IOptions<CorrelatorOptions> options,
             ICorrelationContextFactory correlationContextFactory,
             ICorrelationContextContainer correlationContextContainer,
-            ICorrelationEmitter correlationEmitter)
+            ICorrelationEmitter correlationEmitter,
+            ILogger<CorrelatorMiddleware> logger)
         {
             _next = next;
             _options = options.Value;
             _contextFactory = correlationContextFactory;
             _contextContainer = correlationContextContainer;
             _emitter = correlationEmitter;
+            _logger = logger;
         }
 
         /// <summary>
@@ -48,21 +53,83 @@ namespace W4k.AspNetCore.Correlator
         /// <returns>
         /// A task that represents the execution of this middleware.
         /// </returns>
-        [SuppressMessage("Microsoft.Design", "CA1062", Justification = "If HTTP context is null, we have bigger problem here.")]
         public async Task Invoke(HttpContext httpContext)
         {
             var correlationContext = _contextFactory.CreateContext(httpContext);
             using (_contextContainer.CreateScope(correlationContext))
             {
-                _emitter.Register(httpContext);
+                var correlationId = correlationContext.CorrelationId;
 
-                if (_options.ReplaceTraceIdentifier && correlationContext.CorrelationId != CorrelationId.Empty)
+                RegisterEmitter(_options.Emit, httpContext);
+                ReplaceTraceIdentifier(httpContext, correlationId);
+
+                using (BeginCorrelatedLoggingScope(_options.LoggingScope, correlationId))
                 {
-                    httpContext.TraceIdentifier = correlationContext.CorrelationId;
+                    await _next.Invoke(httpContext);
                 }
-
-                await _next.Invoke(httpContext);
             }
+        }
+
+        /// <summary>
+        /// Registers correlation emitter to <see cref="HttpResponse.OnStarting(Func{object, Task}, object)"/>.
+        /// </summary>
+        /// <param name="propagationSettings">Header propagation settings.</param>
+        /// <param name="httpContext">Current HTTP context.</param>
+        private void RegisterEmitter(PropagationSettings propagationSettings, HttpContext httpContext)
+        {
+            if (propagationSettings.Settings == HeaderPropagation.NoPropagation)
+            {
+                return;
+            }
+
+            httpContext.Response.OnStarting(ctx => _emitter.Emit((HttpContext)ctx), httpContext);
+        }
+
+        /// <summary>
+        /// Replaces <see cref="HttpContext.TraceIdentifier"/> by <see cref="CorrelationId"/> if not empty.
+        /// </summary>
+        /// <param name="httpContext">Current HTTP context.</param>
+        /// <param name="correlationId">Current correlation ID.</param>
+        /// <returns>
+        /// HTTP context where trace identifier is replaced by correlation ID.
+        /// </returns>
+        private HttpContext ReplaceTraceIdentifier(HttpContext httpContext, CorrelationId correlationId)
+        {
+            if (!_options.ReplaceTraceIdentifier || correlationId.IsEmpty)
+            {
+                return httpContext;
+            }
+
+            httpContext.TraceIdentifier = correlationId;
+
+            return httpContext;
+        }
+
+        /// <summary>
+        /// Begins logging scope containing correlation ID.
+        /// </summary>
+        /// <param name="scopeSettings">Logging scope settings.</param>
+        /// <param name="correlationId">Correlation ID.</param>
+        /// <returns>
+        /// Returns new logger scope or <c>null</c> if correlation ID should not be included in scope or when is empty.
+        /// </returns>
+        private IDisposable? BeginCorrelatedLoggingScope(LoggingScopeSettings scopeSettings, CorrelationId correlationId)
+        {
+            if (scopeSettings.IncludeScope)
+            {
+                return null;
+            }
+
+            if (correlationId.IsEmpty)
+            {
+                return null;
+            }
+
+            return _logger.BeginScope(
+                new Dictionary<string, object>
+                {
+                    [scopeSettings.CorrelationKey] = correlationId,
+                });
         }
     }
 }
---
+++ src/W4k.AspNetCore.Correlator/Options/LoggingScopeSettings.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace W4k.AspNetCore.Correlator.Options
+{
+    /// <summary>
+    /// Logging scope settings, <see cref="Microsoft.Extensions.Logging.ILogger.BeginScope{TState}(TState)"/>.
+    /// </summary>
+    public readonly struct LoggingScopeSettings : IEquatable<LoggingScopeSettings>
+    {
+        /// <summary>
+        /// Configure no scope - correlation ID won't be added to logger scope.
+        /// </summary>
+        public static readonly LoggingScopeSettings NoScope = new LoggingScopeSettings(false, string.Empty);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingScopeSettings"/> struct.
+        /// </summary>
+        /// <param name="includeScope">Indicates whether correlation ID should be added to logger scope.</param>
+        /// <param name="key">Correlation ID logger scope key.</param>
+        private LoggingScopeSettings(bool includeScope, string key)
+        {
+            IncludeScope = includeScope;
+            CorrelationKey = key;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether correlation ID should be added to logger scope.
+        /// </summary>
+        public bool IncludeScope { get; }
+
+        /// <summary>
+        /// Gets logger scope key of correlation ID.
+        /// </summary>
+        public string CorrelationKey { get; }
+
+        /// <summary>
+        /// Performs equal comparison between two values.
+        /// </summary>
+        /// <param name="left">Left value.</param>
+        /// <param name="right">Right value.</param>
+        /// <returns>
+        /// Returns <c>true</c> if both values are equal, <c>false</c> otherwise.
+        /// </returns>
+        public static bool operator ==(LoggingScopeSettings left, LoggingScopeSettings right) =>
+            left.Equals(right);
+
+        /// <summary>
+        /// Performs equal comparison between two values.
+        /// </summary>
+        /// <param name="left">Left value.</param>
+        /// <param name="right">Right value.</param>
+        /// <returns>
+        /// Returns <c>true</c> if values differ, <c>false</c> otherwise.
+        /// </returns>
+        public static bool operator !=(LoggingScopeSettings left, LoggingScopeSettings right) =>
+            !left.Equals(right);
+
+        /// <summary>
+        /// Creates setting for including correlation ID within logger scope, using <paramref name="correlationKey"/> as key.
+        /// </summary>
+        /// <param name="correlationKey">Logger scope key.</param>
+        /// <returns>
+        /// New instance of logging scope settings.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="correlationKey"/> is <c>null</c> or empty string.</exception>
+        public static LoggingScopeSettings IncludeLoggingScope(string correlationKey)
+        {
+            if (string.IsNullOrEmpty(correlationKey))
+            {
+                throw new ArgumentNullException(nameof(correlationKey));
+            }
+
+            return new LoggingScopeSettings(true, correlationKey);
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj) =>
+            obj is LoggingScopeSettings other && Equals(other);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+#if NETSTANDARD2_0
+                return 17 * IncludeScope.GetHashCode() ^ CorrelationKey.GetHashCode();
+#else
+                return 17 * IncludeScope.GetHashCode() ^ CorrelationKey.GetHashCode(StringComparison.OrdinalIgnoreCase);
+#endif
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(LoggingScopeSettings other) =>
+            IncludeScope == other.IncludeScope
+            && string.Equals(CorrelationKey, other.CorrelationKey, StringComparison.OrdinalIgnoreCase);
+    }
+}
