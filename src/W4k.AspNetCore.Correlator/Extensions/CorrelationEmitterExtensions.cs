9b9f3a6832d51c7a775c27f5b1d8103de82b93e0
Zdenek Havlin <wdolek@hotmail.com>
2020-08-21 22:42:08 +0200

    Extract `OnStarting` hook to extension method (keeping emitter doing its thing)

---
+++ src/W4k.AspNetCore.Correlator/Extensions/CorrelationEmitterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace W4k.AspNetCore.Correlator.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="ICorrelationEmitter"/>.
+    /// </summary>
+    internal static class CorrelationEmitterExtensions
+    {
+        /// <summary>
+        /// Hooks correlation emitter to HTTP context response.
+        /// </summary>
+        /// <param name="emitter">Correlation emitter.</param>
+        /// <param name="httpContext">HTTP context.</param>
+        public static void Register(this ICorrelationEmitter emitter, HttpContext httpContext) =>
+            httpContext.Response.OnStarting(ctx => emitter.Emit((HttpContext)ctx), httpContext);
+    }
+}
