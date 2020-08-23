6ab36f639108c3e5ea569e0d47c31aaf0b017f09
Zdenek Havlin <wdolek@hotmail.com>
2020-08-24 00:31:50 +0200

    Remove unused `GenerateEmptyHeaders` from `HeaderDictionaryExtensionsTests`

--- test/W4k.AspNetCore.Correlator.UnitTests/Extensions/HeaderDictionaryExtensionsTests.cs
+++ test/W4k.AspNetCore.Correlator.UnitTests/Extensions/HeaderDictionaryExtensionsTests.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Primitives;
+﻿using Microsoft.AspNetCore.Http;
 using W4k.AspNetCore.Correlator.Extensions;
 using Xunit;
 
 namespace W4k.AspNetCore.Correlator.UnitTests.Extensions
 {
-    public class HeaderDictionaryExtensionsTests
+    public class HttpRequestHeadersExtensions
     {
         [Fact]
         public void AddIfNotSet_ExpectHeaderToBeAdded()
@@ -38,21 +36,6 @@ namespace W4k.AspNetCore.Correlator.UnitTests.Extensions
             // assert
             Assert.True(headers.ContainsKey("X-Correlation-ID"));
             Assert.Equal("123", headers["X-Correlation-ID"]);
-        }
-
-        public static IEnumerable<object[]> GenerateEmptyHeaders()
-        {
-            yield return new object[] { new HeaderDictionary(0), "X-Correlation-ID" };
-
-            // silly header value (can this even happen?!)
-            yield return new object[]
-            {
-                new HeaderDictionary
-                {
-                    ["X-Correlation-ID"] = StringValues.Empty
-                },
-                "X-Correlation-ID"
-            };
         }
     }
 }
