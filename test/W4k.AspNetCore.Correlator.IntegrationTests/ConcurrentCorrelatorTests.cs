dee77caf8c638a7f1898ad19e3ab78c079ed030e
Zdenek Havlin <wdolek@hotmail.com>
2020-08-24 16:48:09 +0200

    Introduce new integration test which is sending more requests in parallel (#17)

---
+++ test/W4k.AspNetCore.Correlator.IntegrationTests/ConcurrentCorrelatorTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using W4k.AspNetCore.Correlator.IntegrationTests.Startup;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace W4k.AspNetCore.Correlator.IntegrationTests
+{
+    public sealed class ConcurrentCorrelatorTests : CorrelatorTestsBase<CustomOptionsStartup>
+    {
+        private static readonly Random Random = new Random(12345);
+
+        private readonly ITestOutputHelper _output;
+
+        public ConcurrentCorrelatorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task CorrelationCorrectForEachRequest()
+        {
+            // make sure we make machine sweat
+            var concurrency = Environment.ProcessorCount * 10;
+            _output.WriteLine($"'Concurrency' level: {concurrency} (number of request tasks to be executed)");
+
+            var tasks = Enumerable
+                .Range(0, concurrency)
+                .Select(_ => CreateCorrelatedRequest())
+                .Select((t) => SendRequest(t.Request, t.CorrelationId))
+                .ToArray();
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                var testContext = task.Result;
+                Assert.Equal(testContext.Expected, testContext.Header);
+                Assert.Equal(testContext.Expected, testContext.Body);
+            }
+        }
+
+        private static (HttpRequestMessage Request, string CorrelationId) CreateCorrelatedRequest()
+        {
+            var correlationId = $"IT:{Guid.NewGuid():D}:{DateTime.UtcNow.Ticks}";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Add("X-CID", correlationId);
+
+            return (request, correlationId);
+        }
+
+        private async Task<CorrelationTestContext> SendRequest(HttpRequestMessage request, string expected)
+        {
+            HttpResponseMessage response = await Client.SendAsync(request, CancellationToken.None);
+
+            await Task.Delay(Random.Next(127));
+
+            string headerValue = response.Headers.GetValues("X-CID").FirstOrDefault();
+            string bodyValue = await response.Content.ReadAsStringAsync();
+
+            await Task.Delay(Random.Next(255));
+
+            return new CorrelationTestContext
+            {
+                Header = headerValue,
+                Body = bodyValue,
+                Expected = expected
+            };
+        }
+
+        private class CorrelationTestContext
+        {
+            public string Header { get; set; } = string.Empty;
+            public string Body { get; set; } = string.Empty;
+            public string Expected { get; set; } = string.Empty;
+        }
+    }
+}
---
+++ test/W4k.AspNetCore.Correlator.IntegrationTests/Startup/CustomOptionsStartup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using W4k.AspNetCore.Correlator.Context;
+using W4k.AspNetCore.Correlator.Extensions;
+using W4k.AspNetCore.Correlator.Options;
+
+namespace W4k.AspNetCore.Correlator.IntegrationTests.Startup
+{
+    public class CustomOptionsStartup
+    {
+        public void ConfigureServices(IServiceCollection services) =>
+            services.AddCorrelator(
+                o =>
+                {
+                    // disable correlation ID factory
+                    // (if correlation ID not sent, ASP.NET trace ID is going to be used)
+                    o.Factory = null;
+
+                    // read custom header
+                    o.ReadFrom.Clear();
+                    o.ReadFrom.Add("X-CID");
+
+                    // do not emit correlation ID
+                    o.Emit = PropagationSettings.KeepIncomingHeaderName();
+
+                    // don't overwrite `HttpContext.TraceIdentifier` by correlation ID
+                    o.ReplaceTraceIdentifier = false;
+
+                    // enable logging scope containing correlation ID
+                    o.LoggingScope = LoggingScopeSettings.IncludeLoggingScope("correlation");
+                });
+
+        public void Configure(IApplicationBuilder app, ICorrelationContextAccessor correlationContextAccessor)
+        {
+            app.UseCorrelator();
+            app.Use(async (context, next) =>
+            {
+                var correlationId = correlationContextAccessor.CorrelationContext.CorrelationId;
+
+                context.Response.Headers.Add("X-Test-Correlation-ID", (string)correlationId);
+                context.Response.Headers.Add("Content-Type", "text/plain");
+
+                await context.Response.WriteAsync(correlationId);
+            });
+        }
+    }
+}
