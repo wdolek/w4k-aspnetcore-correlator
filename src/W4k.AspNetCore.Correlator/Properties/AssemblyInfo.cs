93dc92a2f1900c46de48bb98716120d5294ce68e
Zdenek Havlin <wdolek@hotmail.com>
2020-08-25 15:59:00 +0200

    Try-catch correlation scope creation and correlation emit (#22)

--- src/W4k.AspNetCore.Correlator/Properties/AssemblyInfo.cs
+++ src/W4k.AspNetCore.Correlator/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("W4k.AspNetCore.Correlator.IntegrationTests")]
 [assembly: InternalsVisibleTo("W4k.AspNetCore.Correlator.UnitTests")]
---
+++ test/W4k.AspNetCore.Correlator.IntegrationTests/UnbreakableCorrelatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using W4k.AspNetCore.Correlator.Http;
+using W4k.AspNetCore.Correlator.IntegrationTests.Startup;
+using Xunit;
+
+namespace W4k.AspNetCore.Correlator.IntegrationTests
+{
+    public class UnbreakableCorrelatorTests : CorrelatorTestsBase<ThrowingFactoryStartup>
+    {
+        [Fact]
+        public async Task Invoke_WhenFactoryThrows_RequestIsProcessed()
+        {
+            // arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Add(HttpHeaders.AspNetRequestId, "123");
+
+            // act
+            HttpResponseMessage response = await Client.SendAsync(request, CancellationToken.None);
+
+            // assert
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal("OK", responseBody);
+        }
+    }
+}
