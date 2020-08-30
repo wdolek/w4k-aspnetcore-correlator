3d24b095ac5c5e7844ce25d1bbacd3591f04e961
Zdenek Havlin <wdolek@hotmail.com>
2020-08-30 20:54:33 +0200

    Support structured logging by makeing logger state enumerable (#30)

--- src/W4k.AspNetCore.Correlator/Logging/CorrelatedLoggerState.cs
+++ src/W4k.AspNetCore.Correlator/Logging/CorrelatedLoggerState.cs
@@ -1,16 +1,21 @@
-﻿namespace W4k.AspNetCore.Correlator.Logging
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace W4k.AspNetCore.Correlator.Logging
 {
-    internal sealed class CorrelatedLoggerState
+    internal class CorrelatedLoggerState : IEnumerable<KeyValuePair<string, object>>
     {
-        private readonly string _key;
-        private readonly CorrelationId _correlationId;
+        private readonly IReadOnlyList<KeyValuePair<string, object>> _value;
 
         public CorrelatedLoggerState(string key, CorrelationId correlationId)
         {
-            _key = key;
-            _correlationId = correlationId;
+            _value = new[] { new KeyValuePair<string, object>(key, correlationId) };
         }
 
-        public override string ToString() => $"{_key}:{_correlationId.Value}";
+        public override string ToString() => $"{_value[0].Key}:{_value[0].Value}";
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => _value.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _value.GetEnumerator();
     }
 }
