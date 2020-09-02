e475f9d332ff7a1cab30e53a0a0676128240eb55
Zdenek Havlin <wdolek@hotmail.com>
2020-09-02 22:55:25 +0200

    Introduce `ICorrelationValidator` and its implementation

---
+++ src/W4k.AspNetCore.Correlator/Validation/ICorrelationValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace W4k.AspNetCore.Correlator.Validation
+{
+    /// <summary>
+    /// Correlation ID value validator.
+    /// </summary>
+    public interface ICorrelationValidator
+    {
+        /// <summary>
+        /// Validate header value.
+        /// </summary>
+        /// <param name="value">Correlation header value.</param>
+        /// <returns>
+        /// Validation result.
+        /// </returns>
+        ValidationResult Validate(string? value);
+    }
+}
