808dc2648088561f68a113d1bdec8b9e40f182db
Zdenek Havlin <wdolek@hotmail.com>
2020-09-03 23:02:11 +0200

    Add validation benchmarks (happy path)

---
+++ benchmarks/W4k.AspNetCore.Correlator.Benchmarks/Startup/ValidatingCorrelatorStartup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using W4k.AspNetCore.Correlator.Benchmarks.Middleware;
+using W4k.AspNetCore.Correlator.Extensions;
+using W4k.AspNetCore.Correlator.Extensions.DependencyInjection;
+using W4k.AspNetCore.Correlator.Validation;
+
+namespace W4k.AspNetCore.Correlator.Benchmarks.Startup
+{
+    internal class ValidatingCorrelatorStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // all correlation ID values used will be valid
+            services.AddDefaultCorrelator()
+                .WithValidator(new CorrelationValueLengthValidator(40));
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCorrelator();
+            app.UseMiddleware<CorrelatedMiddleware>();
+            app.UseMiddleware<DummyMiddleware>();
+        }
+    }
+}
---
+++ benchmarks/W4k.AspNetCore.Correlator.Benchmarks/ValidatingCorrelatorBenchmarks.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using W4k.AspNetCore.Correlator.Benchmarks.Helpers;
+using W4k.AspNetCore.Correlator.Benchmarks.Startup;
+
+namespace W4k.AspNetCore.Correlator.Benchmarks
+{
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    public class ValidatingCorrelatorBenchmarks : IDisposable
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public ValidatingCorrelatorBenchmarks()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<ValidatingCorrelatorStartup>());
+            _client = _server.CreateClient();
+        }
+
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("Short")]
+        public async Task CorrelatedRequest()
+        {
+            var requestMessage = RequestFactory.CreateCorrelatedRequest();
+            await _client.SendAsync(requestMessage);
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("Long")]
+        public async Task CorrelatedRequestWithAdditionalHeaders()
+        {
+            var requestMessage = RequestFactory.CreateCorrelatedRequestWithAdditionalHeaders();
+            await _client.SendAsync(requestMessage);
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("None")]
+        public async Task RequestWithoutCorrelation()
+        {
+            var requestMessage = RequestFactory.CreateRequestWithoutCorrelation();
+            await _client.SendAsync(requestMessage);
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            _server?.Dispose();
+        }
+    }
+}
