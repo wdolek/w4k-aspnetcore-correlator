e03ed02a5fe560445af290d997fa6a28db5e6175
Zdenek Havlin <wdolek@hotmail.com>
2020-09-04 22:19:25 +0200

    Introduce benchmarks, comparing to `CorrelationId`

---
+++ benchmarks/W4k.AspNetCore.Correlator.Benchmarks/ComparingBenchmarks/TestServerContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.TestHost;
+
+namespace W4k.AspNetCore.Correlator.Benchmarks.ComparingBenchmarks
+{
+    internal class TestServerContainer : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public TestServerContainer(TestServer server)
+        {
+            _server = server;
+            HttpClient = _server.CreateClient();
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public void Dispose()
+        {
+            HttpClient?.Dispose();
+            _server?.Dispose();
+        }
+    }
+}
