0bcb290cb57480402d940dd3671d07860a033e6d
Zdenek Havlin <wdolek@hotmail.com>
2020-09-18 19:56:40 +0200

    Add `net5.0` sample

---
+++ samples/W4k.AspNetCore.Correlator.Example.NetCore5.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace W4k.AspNetCore.Correlator.Example.NetCore50
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddSimpleConsole(options =>
+                    {
+                        options.ColorBehavior = LoggerColorBehavior.Disabled;
+                        options.IncludeScopes = true;
+                        options.UseUtcTimestamp = true;
+                    });
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
