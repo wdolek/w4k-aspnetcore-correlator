e4f437fe4d05f7031c3cde51d39f613990e6fd7f
Zdenek Havlin <wdolek@hotmail.com>
2023-08-30 23:17:21 +0200

    Just use `List<>`, only enumerator is exposed ¯\_(ツ)_/¯

--- src/W4k.AspNetCore.Correlator/Logging/CorrelatedLoggerState.cs
+++ src/W4k.AspNetCore.Correlator/Logging/CorrelatedLoggerState.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace W4k.AspNetCore.Correlator.Logging
 {
-    internal class CorrelatedLoggerState : IEnumerable<KeyValuePair<string, object>>
+    internal sealed class CorrelatedLoggerState : IEnumerable<KeyValuePair<string, object>>
     {
-        private readonly IReadOnlyList<KeyValuePair<string, object>> _value;
+        private readonly List<KeyValuePair<string, object>> _value;
 
         public CorrelatedLoggerState(string key, CorrelationId correlationId)
         {
-            _value = new[] { new KeyValuePair<string, object>(key, correlationId) };
+            _value = new(1) { new KeyValuePair<string, object>(key, correlationId) };
         }
 
         public override string ToString() => $"{_value[0].Key}:{_value[0].Value}";
