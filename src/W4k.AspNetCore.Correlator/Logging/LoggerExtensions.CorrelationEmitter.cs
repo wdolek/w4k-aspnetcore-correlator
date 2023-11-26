fa42526d12a9736ea04290e14a810e49e442b0ca
Zdenek Havlin <wdolek@hotmail.com>
2023-11-26 23:49:56 +0100

    Make `Exception` type parameter nullable for logging

--- src/W4k.AspNetCore.Correlator/Logging/LoggerExtensions.CorrelationEmitter.cs
+++ src/W4k.AspNetCore.Correlator/Logging/LoggerExtensions.CorrelationEmitter.cs
@@ -8,12 +8,12 @@ internal static partial class LoggerExtensions
     private static readonly EventId WritingCorrelatedResponseEvent =
         new EventId(301, nameof(WritingCorrelatedResponse));
 
-    private static readonly Action<ILogger, Exception> LogWritingCorrelatedResponse =
+    private static readonly Action<ILogger, Exception?> LogWritingCorrelatedResponse =
         LoggerMessage.Define(
             LogLevel.Debug,
             WritingCorrelatedResponseEvent,
             "Writing correlation ID to response headers");
 
     public static void WritingCorrelatedResponse(this ILogger logger) =>
-        LogWritingCorrelatedResponse(logger, null!);
+        LogWritingCorrelatedResponse(logger, null);
 }
